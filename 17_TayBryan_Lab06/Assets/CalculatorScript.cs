96a289c9e38bff7cf3a0f0b1dcdae6945f45dc5a
17-TayBryan <votexair@gmail.com>
2021-10-18 19:15:10 +0800

    Finished Calculator Script's functionality

--- 17_TayBryan_Lab06/Assets/CalculatorScript.cs
+++ 17_TayBryan_Lab06/Assets/CalculatorScript.cs
@@ -8,8 +8,12 @@ public class CalculatorScript : MonoBehaviour
 
     float amount;
 
+    int ToggleValue;
+
     float SGDUSD = 0.74f;
     float SGDJPY = 82.78f;
+
+    public Text Debug;
 
     public Toggle toggleUSDollar;
     public Toggle toggleJapYen;
@@ -19,21 +23,78 @@ public class CalculatorScript : MonoBehaviour
 
     public void Start()
     {
-        toggleUSDollar.isOn = false;
-        toggleJapYen.isOn = false;
+        toggleUSDollar.isOn = false; 
+        toggleJapYen.isOn = false;      
     }
 
     public void Update()
     {
-        amount = float.Parse(AmountInput.text);
+        if (toggleJapYen.isOn == true && toggleUSDollar.isOn == true)
+        {
+            ToggleValue = 3;
+        }
+        else if (toggleJapYen.isOn == true)
+        {
+            ToggleValue = 2;
+        }
+        else if (toggleUSDollar.isOn == true)
+        {
+            ToggleValue = 1;
+        }
+        else
+        {
+            ToggleValue = 0;
+        }
+
+        print(ToggleValue);
     }
 
     public void ConvertAmount()
     {
-        ConvertedAmountInput.text = "$" + (amount * SGDUSD);
+       
+        if(ToggleValue == 1)
+        {
+            try
+            {
+                amount = float.Parse(AmountInput.text);
+                ConvertedAmountInput.text = "$" + (amount * SGDUSD);
+            }
+            catch
+            {
+                Debug.text = "Please enter a valid input";
+            }
+            
+        }
+        else if(ToggleValue == 2)
+        {
+            try
+            {
+                amount = float.Parse(AmountInput.text);
+                ConvertedAmountInput.text = "¥" + (amount * SGDJPY);
+            }
+            catch
+            {
+                Debug.text = "Please enter a valid input";
+            }
+            
+        }
+        else if(ToggleValue == 3)
+        {
+            Debug.text = "Please have one currency toggled.";
+        }
+        else
+        {
+            Debug.text = "Please toggle a currency.";
+        }
+       
     }
 
-
-
+    public void ClearInput()
+    {
+        toggleUSDollar.isOn = false;
+        toggleJapYen.isOn = false;
+        ConvertedAmountInput.text = "";
+        AmountInput.text = "";
+    }
 
 }
