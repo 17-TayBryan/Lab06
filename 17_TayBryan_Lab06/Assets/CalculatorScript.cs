8f63f357f125b73526a56e6c6fbfdd93d5f8b344
17-TayBryan <votexair@gmail.com>
2021-10-18 14:52:57 +0800

    Added more functionality to the script

--- 17_TayBryan_Lab06/Assets/CalculatorScript.cs
+++ 17_TayBryan_Lab06/Assets/CalculatorScript.cs
@@ -5,11 +5,32 @@ using UnityEngine.UI;
 
 public class CalculatorScript : MonoBehaviour
 {
+
+    float amount;
+
+    float SGDUSD = 0.74f;
+    float SGDJPY = 82.78f;
+
     public Toggle toggleUSDollar;
+    public Toggle toggleJapYen;
+
+    public InputField AmountInput;
+    public InputField ConvertedAmountInput;
 
     public void Start()
     {
         toggleUSDollar.isOn = false;
+        toggleJapYen.isOn = false;
+    }
+
+    public void Update()
+    {
+        amount = float.Parse(AmountInput.text);
+    }
+
+    public void ConvertAmount()
+    {
+        ConvertedAmountInput.text = "$" + (amount * SGDUSD);
     }
 
 
