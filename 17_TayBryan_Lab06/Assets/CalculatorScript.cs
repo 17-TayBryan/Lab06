58971db922970e2cf51635bccd64bf57e1c8c1bb
17-TayBryan <votexair@gmail.com>
2021-10-18 14:38:22 +0800

    Added calculator script

---
+++ 17_TayBryan_Lab06/Assets/CalculatorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatorScript : MonoBehaviour
+{
+    public Toggle toggleUSDollar;
+
+    public void Start()
+    {
+        toggleUSDollar.isOn = false;
+    }
+
+
+
+
+}
